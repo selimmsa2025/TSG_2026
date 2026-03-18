ce509974de5f4e48701def4340385d3d165f59cd
xoozy0927 <b2uty8453@naver.com>
2026-03-18 16:57:00 +0900

    Update WeatherTool.java

--- SPRING_AI/demo/src/main/java/com/example/demo/tool/WeatherTool.java
+++ SPRING_AI/demo/src/main/java/com/example/demo/tool/WeatherTool.java
@@ -2,12 +2,15 @@ package com.example.demo.tool;
 
 import org.springframework.ai.tool.annotation.Tool;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
+@Slf4j
 public class WeatherTool {
 
     @Tool(description = "도시 이름을 입력하면 날씨 정보를 반환한다.")
     public String getWeather(String city) {
+    	log.info("=== WeatherTool 호출 ===");
         // 실제 API 대신 하드코딩
         if (city.equals("서울")) {
             return "서울의 현재 날씨는 맑고 15도입니다.";
